0e567fa6d9f935541d60cf2d7d28cb473cddd42f
namsu-k <nghokr1111@naver.com>
2022-08-23 15:54:18 +0900

    sep color.js

---
+++ color.js
@@ -0,0 +1,31 @@
+var body = {
+    setColor : function (color) {
+        document.querySelector('body').style.color = color;
+    },
+    setBGColor : function (color) {
+        document.querySelector('body').style.backgroundColor = color;
+    }
+}
+var links = {
+    setColor : function (color) {
+        var alist = document.querySelectorAll('a')
+        var i = 0;
+        while (i < alist.length) {
+            alist[i].style.color = color;
+            i = i + 1;
+        }
+    }
+}
+function dark_mode(self) {
+    if (self.value === 'night') {
+        body.setBGColor("black");
+        body.setColor("white");
+        self.value = 'day';
+        links.setColor("white");
+    } else {
+        body.setBGColor("white");
+        body.setColor("black");
+        self.value = 'night';
+        links.setColor('black');
+    }
+}
