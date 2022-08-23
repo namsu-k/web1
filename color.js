95283c98be6883690dbc979f8d2327f39425fe70
namsu-k <nghokr1111@naver.com>
2022-08-23 16:23:39 +0900

    use jquery

--- color.js
+++ color.js
@@ -1,19 +1,22 @@
 var body = {
     setColor : function (color) {
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $("body").css("color", color);
     },
     setBGColor : function (color) {
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $("body").css("backgroundColor", color);
     }
 }
 var links = {
     setColor : function (color) {
-        var alist = document.querySelectorAll('a')
-        var i = 0;
-        while (i < alist.length) {
-            alist[i].style.color = color;
-            i = i + 1;
-        }
+        // var alist = document.querySelectorAll('a')
+        // var i = 0;
+        // while (i < alist.length) {
+        //     alist[i].style.color = color;
+        //     i = i + 1;
+        // }
+        $('a').css('color', color);
     }
 }
 function dark_mode(self) {
